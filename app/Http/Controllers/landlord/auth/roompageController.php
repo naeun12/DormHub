2d3c299d3d50535f78465862ed9d4e37178145d5
Lance Monsanto <niiinaeun@gmail.com>
2025-10-11 00:43:48 +0800

    update

--- app/Http/Controllers/landlord/auth/roompageController.php
+++ app/Http/Controllers/landlord/auth/roompageController.php
@@ -35,7 +35,7 @@ class roompageController extends Controller
             return redirect()->route('loginLandlord')->with('error', 'Unauthorized access.');
         }
     
-        $landlord = landlordModel::find($landlordId);
+$landlord = landlordModel::where('landlord_id', $landlordId)->first();
         if (!$landlord) {
             return redirect()->route('loginLandlord')->with('error', 'Landlord not found.');
         }
