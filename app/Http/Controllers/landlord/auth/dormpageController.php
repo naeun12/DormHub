cc8312ec8f313ed69b8f89f406219987a7949d25
Lance Monsanto <niiinaeun@gmail.com>
2025-10-11 01:11:59 +0800

    update

--- app/Http/Controllers/landlord/auth/dormpageController.php
+++ app/Http/Controllers/landlord/auth/dormpageController.php
@@ -35,7 +35,7 @@ class dormpageController extends Controller
             return redirect()->route('loginLandlord')->with('error', 'Unauthorized access.');
         }
     
-    $landlord = landlordModel::find($landlordId);
+        $landlord = landlordModel::find($landlordId);
         if (!$landlord) {
             return redirect()->route('loginLandlord')->with('error', 'Landlord not found.');
         }
