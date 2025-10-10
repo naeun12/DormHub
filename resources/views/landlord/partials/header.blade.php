c2a67782b0356fb9f5ed3629421bde14460e2af0
Lance Monsanto <niiinaeun@gmail.com>
2025-10-10 14:40:05 +0800

    Add production build

--- resources/views/landlord/partials/header.blade.php
+++ resources/views/landlord/partials/header.blade.php
@@ -7,7 +7,8 @@
     <title>{{ $title }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/landlordpage/register.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/navigationstyle/landingandacccountprocessnavigation.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/navigationstyle/landingandaccountprocessnavigation.css') }}">
+
     <link rel="icon" href="{{ asset('images/Logo/logo.png') }}" class="rounded" type="image/png">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
