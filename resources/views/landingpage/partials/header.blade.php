9c910803a0d2337d902d79c37674f0f0aa3a031a
naeun12 <143399461+naeun12@users.noreply.github.com>
2025-10-10 11:35:32 +0800

    Update header.blade.php

--- resources/views/landingpage/partials/header.blade.php
+++ resources/views/landingpage/partials/header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dormhub</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="icon" href="{{ asset('images/Logo/logo.png') }}" class="rounded" type="image/png">
 
