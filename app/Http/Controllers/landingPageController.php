506831c24e5ba6a7eaa44ed040d0a8e6158e3e63
Lance Monsanto <niiinaeun@gmail.com>
2025-10-10 21:03:02 +0800

    Update landingPageController and routes to use SendGrid API for email sending

--- app/Http/Controllers/landingPageController.php
+++ app/Http/Controllers/landingPageController.php
@@ -16,14 +16,28 @@ class landingPageController extends Controller
     }
     public function sendEmail(Request $request)
     {
-         $request->validate([
+        $request->validate([
             'name'    => 'required|string|max:255',
             'email'   => 'required|email',
             'message' => 'required|string|max:1000',
         ]);
-        $tenantName = $request->name;
-        $messageBody = $request->message;
-        Mail::to($request->email)->send(new userEmail($tenantName, $messageBody));
+
+        $tenantEmail = $request->email;
+        $messageBody = "Hi " . $request->name . ",\n\n" . $request->message;
+        $email = new Mail();
+        $email->setFrom("niiinaeun@gmail.com", "DormHub");
+        $email->setSubject("DormHub Reminder");
+        $email->addTo($tenantEmail);
+        $email->addContent("text/plain", $messageBody);
+
+        try {
+            $sendgrid = new SendGrid(env('SENDGRID_API_KEY'));
+            $response = $sendgrid->send($email);
+            \Log::info("SendGrid Status: " . $response->statusCode());
+        } catch (\Exception $e) {
+            \Log::error("SendGrid API Error: " . $e->getMessage());
+        }
+
         return back()->with('success', 'Your message has been sent successfully!');
 
 
