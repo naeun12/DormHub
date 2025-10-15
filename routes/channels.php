f176aed88117aebee749e33590c55b0da5e8dd85
Lance Monsanto <niiinaeun@gmail.com>
2025-10-15 12:33:48 +0800

    Update

--- routes/channels.php
+++ routes/channels.php
@@ -4,12 +4,11 @@ use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Auth;
 use App\Models\conversationModel;
 
-
 Broadcast::channel('notifications.{receiverID}', function ($user, $receiverID) {
     return (string) ($user->landlordID ?? $user->tenantID) === (string) $receiverID;
 }, ['guards' => ['landlord', 'tenant']]);
 Broadcast::channel('chat.{conversationId}', function ($user, $conversationId) {
-    $conversation = \App\Models\conversationModel::find($conversationId);
+    $conversation = \App\Models\ConversationModel::find($conversationId);
 
     if (!$conversation) {
         return false;
